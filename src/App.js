851b9bad22db83fcdd28b442592ab789d7beddd2
N.Truong244 <n.truongtp@gmail.com>
2022-07-02 10:56:45 +0700

    feat: Add router for app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {Container, CssBaseline} from "@mui/material";
+import {LoginPage} from "./features/login/LoginPage";
+import {InitProvider} from "./features/initProvider/InitProvider";
+import {Route, Switch} from "react-router-dom";
+import {useSelector} from "react-redux";
+import {HomePage} from "./features/home/HomePage";
+import {NewPollPage} from "./features/newPoll/NewPollPage";
+import {NavBar} from "./components/NavBar/NavBar";
+import {AnswerPollPage} from "./features/answerPoll/AnswerPollPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const {currentUser} = useSelector(state => state.auth)
+
+    return (
+        <InitProvider>
+            <Container maxWidth="md" sx={{display: "flex", justifyContent: "center", flexWrap: "wrap", mb: 2}}>
+                <CssBaseline/>
+                {currentUser !== null && <NavBar />}
+                <Switch>
+                    {currentUser === null && <Route path="/*" component={LoginPage} />}
+                    <Route path="/questions/:questionId" component={AnswerPollPage} />
+                    <Route path="/add" component={NewPollPage} />
+                    <Route path="/" component={HomePage} />
+                </Switch>
+            </Container>
+        </InitProvider>
+    );
 }
 
 export default App;
